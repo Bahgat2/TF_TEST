93f6a49d0e348a333c7ba4798c1c4389b0aa324c
Bahgat2 <an.emperor@yahoo.com>
2024-01-18 22:31:36 +0200

    commit-7

--- variable.tf
+++ variable.tf
@@ -1,15 +1,15 @@
-variable "ami" {
-    type = string
+# variable "ami" {
+#     type = string
 
-}
-variable "instance_type" {
-  type = string
-}
-variable "access_key" {
-    type = string
+# }
+# variable "instance_type" {
+#   type = string
+# }
+# variable "access_key" {
+#     type = string
   
-}
-variable "secret_key" {
-    type = string
+# }
+# variable "secret_key" {
+#     type = string
   
-}
+# }
