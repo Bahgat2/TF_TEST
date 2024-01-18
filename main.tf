77a108407c911521893243e20b8109a42ab05bf1
Bahgat2 <an.emperor@yahoo.com>
2024-01-18 22:36:23 +0200

    commit-9

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
-# terraform {
-#   required_providers {
-#     aws ={
-#         source = "hashicorp/aws"
-#         version = "~>4.16"
-#     }
-#   }
-# }
-# provider "aws" {
-#     region = "us-east-2"
-#     access_key = var.access_key
-#     secret_key = var.secret_key
+terraform {
+  required_providers {
+    aws ={
+        source = "hashicorp/aws"
+        version = "~>4.16"
+    }
+  }
+}
+provider "aws" {
+    region = "us-east-2"
+    access_key = var.access_key
+    secret_key = var.secret_key
   
-# }
-# resource "aws_instance" "pc_vcm" {
-#   ami = var.ami
-#   instance_type = var.instance_type
-#   tags = {
-#     dep = "PKI"
-#   }
-# }
-# resource "aws_iam_user" "tota" {
-#   name = "tota"
-#   tags = {
-#     haya = "shatora"
-#   }
-# }
+}
+resource "aws_instance" "pc_vcm" {
+  ami = var.ami
+  instance_type = var.instance_type
+  tags = {
+    dep = "PKI"
+  }
+}
+resource "aws_iam_user" "tota" {
+  name = "tota"
+  tags = {
+    haya = "shatora"
+  }
+}
